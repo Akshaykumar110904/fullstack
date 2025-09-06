5028e8f1f5f052529a251856157f8e78b6e2ad02
2203A54049 <2203a54049@sru.edu.in>
2025-09-06 06:22:36 +0530

    added Tailwindcss to React application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState} from 'react'
+import { useState,useEffect} from 'react'
 import axios from 'axios';
-
 function App() {
 
   const [users,setUsers]=useState([]);
@@ -28,28 +27,47 @@ function App() {
       setForm({name:"",email:"",age:""});
       fetchUsers();
     }catch(error){
+
       console.error("Error adding user:",error);
     }
   }
+  useEffect(() => {
+    fetchUsers();
+  }, []); 
   return (
     <>
-      <h1>Express + MongoDB + React</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Full Name : </label>
-        <input type="text" name='name' placeholder='enter your name' onChange={handleChange} value={form.name}/><br />
-        <label htmlFor="email">Email : </label>
-        <input type="email" name="email" placeholder='enter your email' onChange={handleChange} value={form.email} /><br />
-        <label htmlFor="age">Age : </label>
-        <input type="number" name="age" placeholder='enter your age' onChange={handleChange} value={form.age}/><br />
-        <button type="submit">Add User</button>
-      </form>
-      <h2>Users List</h2>
-      <button onClick={fetchUsers}>Display</button>
-      <ul>
-        {users.map((user)=>(
-          <li key={user._id}>{user.name} - {user.email} - {user.age}</li>
-        ))}
-      </ul>
+      <div className="flex flex-col items-center justify-center bg-black h-full">
+        <h1 className='text-green-500 text-5xl m-5 p-5'>Express + MongoDB + React</h1>
+        <form onSubmit={handleSubmit} className='bg-sky-400 min-h-0.5 w-2/3 m-5 p-5 text-2xl flex flex-col items-center justify-between '>
+          <div>
+          <label htmlFor="name" className='text-orange-600 m-5 p-5 bg-transparent hover:text-orange-700 text-3xl'>Full Name : </label>
+          <input type="text" name='name' placeholder='enter your name' onChange={handleChange} value={form.name} required className='bg-gray-500 p-5 m-5 w-auto h-4'/><br />
+          </div>
+          <div>
+            <label htmlFor="email" className='text-orange-600 m-5 p-5 bg-transparent hover:text-orange-700 text-3xl'>Email : </label>
+            <input type="email" name="email" placeholder='enter your email' onChange={handleChange} value={form.email} required className='bg-gray-500 p-5 m-5 w-auto h-4' /><br />
+          </div>
+          <div>
+            <label htmlFor="age" className='text-orange-600 m-5 p-5 bg-transparent hover:text-orange-700 text-3xl'>Age : </label>
+            <input type="number" name="age" placeholder='enter your age' onChange={handleChange} value={form.age} required className='bg-gray-500 p-5 m-5 w-auto h-4'/><br />
+          </div>
+          <button type="submit" className='m-3 p-3 bg-green-400 rounded-xl hover:bg-green-500 hover:rounded-full hover:ease-linear'>Add User</button>
+          <div id='error'></div>
+        </form>
+      </div>
+      <div className='flex flex-col items-center justify-center bg-black'>
+        <h2 className='text-4xl text-red-500'>Users List</h2>
+        <button onClick={fetchUsers} className='bg-indigo-500 m-3 p-3 w-auto h-auto rounded-lg'>Display</button>
+        <div className='flex flex-wrap items-center justify-center m-5 p-5 '>
+          {users.map((user)=>(
+            <div className='bg-slate-400 m-4 p-4 rounded-xl font-mono'>
+                <h3 className='text-3xl font-bold text-blue-500 hover:text-indigo-600'>Name : {user.name}</h3>
+                <h4 className='text-xl font-semibold hover:text-black'>Email : {user.email}</h4>
+                <h4 className='text-lg font-medium hover:text-black'>Age : {user.age}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
